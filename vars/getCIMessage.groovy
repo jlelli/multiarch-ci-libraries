eab25b535f2f5e49d64c4c8957b06354fdd95e58
Juri Lelli <juri.lelli@redhat.com>
2018-01-09 20:00:01 +0100

    vars: try to print CI_MESSAGE content to console

--- vars/getCIMessage.groovy
+++ vars/getCIMessage.groovy
@@ -1,23 +1,41 @@
 import groovy.json.*
-import hudson.model.*
+//import hudson.model.*
+//import hudson.util.*
+//import java.util.logging.*
 
-def call() {
-  //def out
-  //def config = new HashMap()
-  //def bindings = getBinding()
-  //config.putAll(bindings.getVariables())
-  //out = config['out']
-  //out.println "Printed to Jenkins console."
-  echo "Printed to Jenkins console."
-  
-  //def slurper = new JsonSlurper()
-  //def json = slurper.parseText(build.getEnvironment(listener).get('CI_MESSAGE'))
-  //out.println json
-  //if (json["build"] != null) {
-  //    String taskId = json['build'].task_id
-  //    def pa = new ParametersAction([new StringParameterValue("TASK_ID", taskId)])
-  //    Thread.currentThread().executable.addAction(pa)
-  //}
+//def call() {
+//  echo "Content of the CI_MESSAGE:"
+//  
+//  // Retrieve parameters of the current build
+//  //def msg = System.getenv("CI_MESSAGE")
+//  //println "CI_MESSAGE=$msg"
+//
+//  //def listener = new LogTaskListener(Logger.getAnonymousLogger(), Level.OFF)
+//  def slurper = new JsonSlurper()
+//  def json = slurper.parseText(Run.getEnvironment(listener).get('CI_MESSAGE'))
+//  println "CI_MESSAGE=$json"
+//  //if (json["build"] != null) {
+//  //    String taskId = json['build'].task_id
+//  //    def pa = new ParametersAction([new StringParameterValue("TASK_ID", taskId)])
+//  //    Thread.currentThread().executable.addAction(pa)
+//  //}
+//
+//  //def resolver = AbstractBuild.getBuildVariableResolver()
+//  //def slurper = new JsonSlurper()
+//  //def json = slurper.parseText(resolver.resolve('CI_MESSAGE'))
+//  //println "CI_MESSAGE=$json"
+//}
+//node {
+//    deleteDir()
+//    stage('test1') {
+//        slurpIt(env.CI_MESSAGE)
+//
+//        sh 'env | sort'
+//    }
+//}
+
+def call(String message) {
+
+    def json = new JsonSlurper().parseText(message)
+    println "CI_MESSAGE=$message"
 }
-
-
